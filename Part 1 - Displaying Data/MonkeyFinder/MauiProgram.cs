43dc2f447e864e448ee6654313749ca8128326e2
Opilane TTHK <dmkotkas@gmail.com>
2023-05-25 14:15:52 +0300

    Add services

--- Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
+++ Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using MonkeyFinder.View;
+using MonkeyFinder.Services;
 
 namespace MonkeyFinder;
 
@@ -19,6 +20,8 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
+        builder.Services.AddSingleton<MonkeyService>();
+        builder.Services.AddSingleton<MonkeysViewModel>(); 
 		builder.Services.AddSingleton<MainPage>();
 
 		return builder.Build();
