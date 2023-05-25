13db4fa8bee225fd36b6be199e3c8c3c91bae3d2
Opilane TTHK <dmkotkas@gmail.com>
2023-05-25 14:13:33 +0300

    Add call from view model

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,43 @@
 ﻿namespace MonkeyFinder.ViewModel;
+using MonkeyFinder.Services;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
